0bf35ef3fa626bf2285965dbe59acaeb7820ccfd
Sander <97965051+GDS-Sander@users.noreply.github.com>
2023-09-02 16:16:12 +0200

    Add files via upload

---
+++ sanCore/commands/AICommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using sanCore.Commands;
+
+namespace sanCore
+{
+    public static class AICommand
+    {
+        private static bool isAIActivated = false;
+
+        public static void ActivateAI()
+        {
+            if (!isAIActivated)
+            {
+                AI.Brain ai = new AI.Brain();
+                Console.WriteLine("AI activated. You can ask me any question. Type 'exit' to exit.");
+                TalkToAI(ai);
+
+                Console.WriteLine("AI deactivated.");
+                isAIActivated = false;
+            }
+            else
+            {
+                Console.WriteLine("AI is already activated.");
+            }
+        }
+
+
+
+
+        private static void TalkToAI(AI.Brain ai)
+        {
+            string question;
+            do
+            {
+                Console.Write("You: ");
+                question = Console.ReadLine();
+                if (question.ToLower() != "exit")
+                {
+                    string answer = ai.AnswerQuestion(question);
+                    Console.WriteLine("AI System: " + answer);
+                }
+            } while (question.ToLower() != "exit");
+        }
+    }
+}
